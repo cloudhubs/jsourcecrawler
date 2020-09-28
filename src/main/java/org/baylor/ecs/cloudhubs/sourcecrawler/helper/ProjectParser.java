b85d3c5b1fa8eb203cab8ce377a9be8012ec907b
nnazo <jacobcurtis2266@gmail.com>
2020-09-27 23:24:43 -0500

    load classes into Scene when constructing ProjectParser

--- src/main/java/org/baylor/ecs/cloudhubs/sourcecrawler/helper/ProjectParser.java
+++ src/main/java/org/baylor/ecs/cloudhubs/sourcecrawler/helper/ProjectParser.java
@@ -3,21 +3,25 @@ package org.baylor.ecs.cloudhubs.sourcecrawler.helper;
 import lombok.Getter;
 import lombok.Value;
 import soot.*;
+import soot.options.Options;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Value
 public class ProjectParser {
     @Getter
-    private final SourceLocator srcLocator;
+    SourceLocator srcLocator;
     @Getter
-    private final List<SootClass> sootClasses;
+    List<SootClass> sootClasses;
 
     public ProjectParser(String projectRoot) {
         super();
         srcLocator = SourceLocator.v();
         Scene.v().setSootClassPath(projectRoot);
+        Scene.v().loadNecessaryClasses();
         var classes = srcLocator.getClassesUnder(projectRoot);
         var classSources = new ArrayList<ClassSource>();
 
